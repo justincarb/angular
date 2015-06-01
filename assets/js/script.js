8adc8ef0496a51cbf6aeaeef49a4c486622c066c
Amrit Singh <direcode@gmail.com>
2015-05-31 19:30:56 -0500

    boostrap added

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var app = angular.module('myApp' , []);
+
+app.controller('nameCtrl' , function($scope){
+    $scope.firstname = "";
+    $scope.lastname = "";
+    $scope.addthis = function(){
+
+    }
+});
